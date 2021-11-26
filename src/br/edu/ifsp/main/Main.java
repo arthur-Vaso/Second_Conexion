a4c6907d3f764e681796a7dcc2f502cdf8c6abe2
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2021-11-26 11:40:23 -0300

    The Main class was created to run the program.

---
+++ src/br/edu/ifsp/main/Main.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.main;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = 
+				Persistence.createEntityManagerFactory("databasePU");
+		
+		emf.close();
+		
+	}
+
+}

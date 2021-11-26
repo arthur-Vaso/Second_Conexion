9c614b9804d155d78262c6ccb1df2d71c6d31b44
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2021-11-26 11:18:08 -0300

    The Person class was created with its attributes and prepares them for use by Hibernate. Also created META xml to define the use of Hibernate.
    This was done during class.

---
+++ src/br/edu/ifsp/model/Person.java
@@ -0,0 +1,60 @@
+package br.edu.ifsp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pessoa")
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="idade")
+	private int idade;
+	
+	@Column(name="endereco")
+	private String endereco;
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+}

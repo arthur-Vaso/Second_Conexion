79892e5b2356269c204fc56855d0ee93fae57f3f
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2021-12-10 10:19:46 -0300

    All persistence holders were eliminated.

--- src/br/edu/ifsp/main/Main.java
+++ src/br/edu/ifsp/main/Main.java
@@ -1,106 +1,34 @@
 package br.edu.ifsp.main;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import br.edu.ifsp.model.Person;
+import br.edu.ifsp.personDAO.PersonDAO;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		/*
-		 * Criando conexão com a base de dados a partir das configurações da unidade de
-		 * persistência no persistence.xml
-		 */
-		/*
-		 * Creating connection with database from persistence configuration unity of
-		 * persistence.xml
-		 */
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("databasePU");
+		Person p1 = new Person();
+		p1.setId(2);
+		p1.setName("João Pedro");
+		p1.setYearOld(54);
+		p1.setAdress("Rua das Flores");
 
-		// Criando o gerenciador de entidade para persistir as entidades
-		// Creating management unity for persistence entity
-		EntityManager em = emf.createEntityManager();
-
-		// Para persist(), merge() e remove(), usam begin() e commit() e para find() não
-		// For persist(), merge() e remove(), they use begin() and commit(), and for
-		// find() don`t
-
-		/*
-		 * Bloco de inserção (insert block)
-		 */
-		// Person p1 = new Person();
-		// p1.setName("João Pedro Alcantara");
-		// p1.setYearOld(54);
-		// p1.setAdress("Rua das Flores");
-
-		// Abrindo uma transação
-		// Opening one transaction
-		// em.getTransaction().begin();
-
-		// Executando operação com o objeto na base de dados
-		// Executing an operation with object in database
-
-		// Inserindo entidade na base de dados
-		// Inserting an entity in database
-		// em.persist(p1);
-
-		// Executar a transação
-		// Executing the transaction
-		// em.getTransaction().commit();
-
-		/*
-		 * Bloco de busca (Fetch block)
-		 */
-		// Person p1 = new Person();
-		// p1.setId(1); p1.setName("João Pedro Alcantara");
-		// p1.setYearOld(54);
-		// p1.setAdress("Rua das Flores");
-
-		// Buscar a entidade na base de dados
-		// Fetch an entity in database
-		// Person p1 = em.find(Person.class, 1);
-
-		/*
-		 * Bloco para atualizar (Actualization block)
-		 */
-		// Opening one transaction
-		// em.getTransaction().begin();
-		// Atualizar entidade na base de dados
-		// Update an entity in database
-		// em.merge(p1);
-		// em.getTransaction().commit();
-
-		/*
-		 * Bloco para exclusão (Deleting block)
-		 */
-		// Person p1 = new Person();
-		// p1.SetId(1);
-		// p1.setName("João Pedro Alcantara");
-		// p1.setYearOld(54);
-		// p1.setAdress("Rua das Flores");
-
-		// Usado para a operação de exclusão
-		// Used for delete operation
-		Person p1 = em.find(Person.class, 1);
-
-		em.getTransaction().begin();
-		// Excluir uma entidade
-		// Remove an entity from database
-		em.remove(p1);
-		em.getTransaction().commit();
-		System.out.println("Nome pessoa: " + p1.getName());
-
-		// Fechando o gerenciador de entidade
-		// Closing the entity management
-		em.close();
-
-		// fechando a conexão
-		// Closing connection
-		emf.close();
-
+		PersonDAO dao = new PersonDAO();
+		//persist
+		p1 = dao.savePerson(p1);
+		System.out.println(p1.getId() + " -" + p1.getName() + " -" + p1.getYearOld() + " -" + p1.getAdress());
+		
+		p1.setName("Carlos");
+		p1.setYearOld(60);
+		p1.setAdress("Rua da Vossa Ovelina");
+		//merge
+		p1 = dao.savePerson(p1);
+		System.out.println(p1.getId() + " -" + p1.getName() + " -" + p1.getYearOld() + " -" + p1.getAdress());
+		
+		//find
+		Person p2 = dao.fetchPersonById(2);
+		System.out.println(p2.getId() + " -" + p2.getName() + " -" + p2.getYearOld() + " -" + p2.getAdress());
+		dao.removePerson(p2.getId());
 	}
 
 }

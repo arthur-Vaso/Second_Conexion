5875a45bb2aa2afccab342b51c6e9e81fd41e97e
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2021-12-17 10:16:28 -0300

    Added classes Pets and Vehicle with hybernate annotation.

--- src/br/edu/ifsp/model/Person.java
+++ src/br/edu/ifsp/model/Person.java
@@ -1,10 +1,15 @@
 package br.edu.ifsp.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -23,6 +28,28 @@ public class Person {
 
 	@Column(name = "adress")
 	private String adress;
+
+	@OneToOne(cascade=CascadeType.ALL)
+	private Vehicle vehicle;
+	
+	@OneToMany(cascade=CascadeType.ALL, mappedBy = "person")
+	private List<Pets> listPets;
+	
+	public List<Pets> getListPets() {
+		return listPets;
+	}
+
+	public void setListPets(List<Pets> listPets) {
+		this.listPets = listPets;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
 
 	public Integer getId() {
 		return id;

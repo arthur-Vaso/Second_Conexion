84b35ac6905553a10c95d2962f2317e94fb94766
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2021-12-17 10:17:46 -0300

    Added new classes in DB

--- src/br/edu/ifsp/main/Main.java
+++ src/br/edu/ifsp/main/Main.java
@@ -1,6 +1,11 @@
 package br.edu.ifsp.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.edu.ifsp.model.Person;
+import br.edu.ifsp.model.Pets;
+import br.edu.ifsp.model.Vehicle;
 import br.edu.ifsp.personDAO.PersonDAO;
 
 public class Main {
@@ -8,16 +13,36 @@ public class Main {
 	public static void main(String[] args) {
 
 		Person p1 = new Person();
-		p1.setId(2);
-		p1.setName("João Pedro");
-		p1.setYearOld(54);
-		p1.setAdress("Rua das Flores");
+		p1.setName("João");
+		p1.setYearOld(12);
+		p1.setAdress("Rua das");
+		
+		Vehicle v = new Vehicle();
+		v.setType("Carro Fusca");
+		v.setPerson(p1);
+		
+		p1.setVehicle(v);
+		
+		Pets a1 = new Pets();
+		a1.setPetName("Totó");
+		a1.setPetType("Cão");
+		a1.setPerson(p1);
+		Pets a2 = new Pets();
+		a2.setPetName("Bichano");
+		a2.setPetType("Gato");
+		a2.setPerson(p1);
+				
+		List<Pets> lisPet = new ArrayList<Pets>();
+		lisPet.add(a1);
+		lisPet.add(a2);
 
+		p1.setListPets(lisPet);
+		
 		PersonDAO dao = new PersonDAO();
 		//persist
 		p1 = dao.savePerson(p1);
-		System.out.println(p1.getId() + " -" + p1.getName() + " -" + p1.getYearOld() + " -" + p1.getAdress());
-		
+		//System.out.println(p1.getId() + " -" + p1.getName() + " -" + p1.getYearOld() + " -" + p1.getAdress());
+		/*
 		p1.setName("Carlos");
 		p1.setYearOld(60);
 		p1.setAdress("Rua da Vossa Ovelina");
@@ -29,6 +54,7 @@ public class Main {
 		Person p2 = dao.fetchPersonById(2);
 		System.out.println(p2.getId() + " -" + p2.getName() + " -" + p2.getYearOld() + " -" + p2.getAdress());
 		dao.removePerson(p2.getId());
+		*/
 	}
 
 }
